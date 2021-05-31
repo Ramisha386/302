b061d35be6e35b64245f250f34ee25b73e064d16
Mouneeta <mouneetar@gmail.com>
2021-05-31 22:36:24 +0600

    k

--- partials/_nav.php
+++ partials/_nav.php
@@ -63,20 +63,19 @@
                                 <div class="modal-body">
                                     <form action="/examples/actions/confirmation.php" method="post">
                                     <div class="form-group">
-                                    <label class="checkbox-inline"><input type="checkbox" required="required"> USER</label>
+                                    <label for="vehicle1"> ADMIN</label><br>
+                                    <input type="checkbox" id="vehicle2" name="admin" value="admin">
+                                     <label for="vehicle2"> USER</label><br>
+                                     <input type="checkbox" id="vehicle3" name="user" value="user">
+                                    <div class="form-group">
+                                            <input type="text" class="form-control" name="username" placeholder="Username" required="required">		
                                     </div>
                                     <div class="form-group">
-                                    <label class="checkbox-inline"><input type="checkbox" required="required"> ADMIN</label>
-                                    </div>
-                                        <div class="form-group">
-                                            <input type="text" class="form-control" name="username" placeholder="Username" required="required">		
-                                        </div>
-                                        <div class="form-group">
                                             <input type="password" class="form-control" name="password" placeholder="Password" required="required">	
-                                        </div>        
-                                        <div class="form-group">
+                                    </div>        
+                                    <div class="form-group">
                                             <button type="submit" class="btn btn-primary btn-lg btn-block login-btn">Login</button>
-                                        </div>
+                                    </div>
                                     </form>
                                 </div>
                                 <div class="modal-footer">
