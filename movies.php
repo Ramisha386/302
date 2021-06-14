5360b9f09d520ea89c900dfa36616398558a87d3
Riyadil <riyadilzannat201914054@gmail.com>
2021-06-14 18:41:29 +0600

    details movie

--- movies.php
+++ movies.php
@@ -184,91 +184,7 @@
 
 <body>
     
-	<!--<header id="header">      
-        
-        <div class="navbar navbar-inverse" role="banner">
-            <div class="container">
-                <div class="navbar-header">
-                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-                        <span class="sr-only">Toggle navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
 
-                    <a class="navbar-brand" href="index.html">
-                        <h1><img src="images/logo1.jpg" alt="logo" width="149" height="76"></h1>
-                    </a>
-                    
-                </div>
-                <div class="collapse navbar-collapse">
-                    <ul class="nav navbar-nav navbar-right">
-                        <li><a href="index.html">Home</a></li>
-                        <li class="dropdown active"><a href="#">Pages <i class="fa fa-angle-down"></i></a>
-                            <ul role="menu" class="sub-menu">
-                                <li><a class="active" href="aboutus.html">Movies</a></li>
-                                <li><a href="aboutus2.html">About 2</a></li>
-                                <li><a href="service.html">Services</a></li>
-                                <li><a href="pricing.html">Pricing</a></li>
-                                <li><a href="contact.html">Contact us</a></li>
-                                <li><a href="contact2.html">Contact us 2</a></li>
-                                <li><a href="404.html">404 error</a></li>
-                                <li><a href="coming-soon.html">Coming Soon</a></li>
-                            </ul>
-                        </li>                    
-                        <li class="dropdown"><a href="blog.html">Blog <i class="fa fa-angle-down"></i></a>
-                            <ul role="menu" class="sub-menu">
-                                <li><a href="blog.html">Blog Default</a></li>
-                                <li><a href="blogtwo.html">Timeline Blog</a></li>
-                                <li><a href="blogone.html">2 Columns + Right Sidebar</a></li>
-                                <li><a href="blogthree.html">1 Column + Left Sidebar</a></li>
-                                <li><a href="blogfour.html">Blog Masonary</a></li>
-                                <li><a href="blogdetails.html">Blog Details</a></li>
-                            </ul>
-                        </li>
-                        <li class="dropdown"><a href="portfolio.html">Portfolio <i class="fa fa-angle-down"></i></a>
-                            <ul role="menu" class="sub-menu">
-                                <li><a href="portfolio.html">Portfolio Default</a></li>
-                                <li><a href="portfoliofour.html">Isotope 3 Columns + Right Sidebar</a></li>
-                                <li><a href="portfolioone.html">3 Columns + Right Sidebar</a></li>
-                                <li><a href="portfoliotwo.html">3 Columns + Left Sidebar</a></li>
-                                <li><a href="portfoliothree.html">2 Columns</a></li>
-                                <li><a href="portfolio-details.html">Portfolio Details</a></li>
-                            </ul>
-                        </li>                         
-                        <li><a href="shortcodes.html ">Shortcodes</a></li>                    
-                    </ul>
-                </div>
-                <div class="search">
-                    <form role="form">
-                        <i class="fa fa-search"></i>
-                        <div class="field-toggle">
-                            <input type="text" class="search-form" autocomplete="off" placeholder="Search">
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </header>-->
-    <!--/#header-->
-     <!-- 
-    <section id="page-breadcrumb">
-        <div class="vertical-center sun">
-             <div class="container">
-                <div class="row">
-                    <div class="action">
-                        <div class="col-sm-12">
-                            <h1 class="title">Lights Camera Action</h1>
-                            <p>We offer an unparallel movie experience that will blow your mind.
-
-                            </p>
-                        </div>
-                     </div>
-                </div>
-            </div>
-        </div>
-   </section>
-   -->
    <header id="header">      
         <div class="container">
             <div class="row">
@@ -355,7 +271,7 @@
                 </div>
                     <br>
                     <br>
-                    <a href="#" class="myButton">Buy Ticket</a>
+                    <a href="movie-1.php" class="myButton">Buy Ticket</a>
                     <a href="https://www.youtube.com/watch?v=tI1JGPhYBS8" class="myButton">Trailer</a>
             </div>
         </div>
@@ -486,7 +402,7 @@
     <div class="person-info col-sm-6">
         <h2 class="headings">Black Widow</h2>
         <div class="description">
-        <p>2021 ‧ Adventure/Action ‧ 2h 13m</p>
+        <p>2019 ‧ Action/Sci-fi ‧ 3h 2m</p>
     
         <p>At birth the Black Widow (aka Natasha Romanova) is given to the KGB, which grooms her to become its ultimate operative. When the U.S.S.R. breaks up, the government tries to kill her as the action moves to present-day New York, where she is a freelance operative.
             <br>
