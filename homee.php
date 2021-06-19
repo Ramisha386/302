cf5b2278de14e3e7b5013790a71e059b5b09ff76
Ramisha386 <ramishabaki@gmail.com>
2021-06-19 15:47:14 +0600

    home edit

--- homee.php
+++ homee.php
@@ -35,6 +35,10 @@ session_start();
    <link rel="shortcut icon" type="imagge/png" href="images/favicon/camera.png">
    <link rel="preconnect" href="https://fonts.gstatic.com">
 <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@500&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@700&family=Lobster&display=swap" rel="stylesheet">
+<link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@700&family=Lobster&family=Yanone+Kaffeesatz&display=swap" rel="stylesheet">
+
   <style>
         * {
             padding: 0;
@@ -224,6 +228,165 @@ session_start();
         {
             color: white;
         }
+        .banner-area
+        {
+            background-position:center center;
+            -webkit-background-size:cover;
+            background-size:cover;
+            height:10vh;
+           margin-bottom:150px;
+           margin-top:100px;
+
+        }
+        .banner-text
+        {
+            position:absolute;
+            top:50%;
+            left:50%;
+            transform:translate(-50%,-50%);
+            color:#fff;
+            text-align:center;
+            
+        }
+
+        .banner-text h2
+        {
+            margin:0;
+            font-size:55px;
+            color:#FAF3F3;
+            font-family: 'Yanone Kaffeesatz', sans-serif;
+        }
+        .banner-text p
+        {
+            font-size:22px;
+            margin:0;
+            font-family: 'Lobster', cursive;
+        }
+        .banner-text a
+        {
+            text-decoration:none;
+            border:1px solid #fff;
+            font-size: 17px;
+            padding:10px 35px;
+            color:#fff;
+            display:inline-block;
+            margin-top:5%;
+            font-family: 'Dancing Script', cursive;
+            text-transform:uppercase;
+            border-color:darkred;
+        }
+        .banner-text a:hover
+        {
+           border-color:white;
+           background:#DA0037;
+           transition:.9s;
+           color:antiquewhite;
+        }
+        
+        .logo h2
+        {
+            padding:20px 0;
+            width:100%;
+            font-size:50px;
+            font-family: 'Lobster', cursive;
+        }
+        .blog-post
+        {
+            width:100%;
+            max-width:98rem;
+            padding:5rem;
+            background-color:#630000;
+            box-shadow:0 1.4rem 8rem rgba(0,0,0,.2);
+            display:flex;
+            align-items:center;
+            border-radius:.8rem;
+            margin:auto;
+            opacity:0.8;
+        }
+
+        .blog-post_img
+        {
+            min-width: 35rem;
+            max-width: 35rem;
+            height:30rem;
+            transform: translateX(-8rem);
+            position:relative;
+            opacity:1;
+        }
+
+        .blog-post_img img
+        {
+            widht:100%;
+            height:100%;
+            object-fit:cover;
+            display:block;
+            border-radius:.8rem;
+            opacity:1;
+        }
+        .blog-post_img img::before
+        {
+            content:'';
+            widht:100%;
+            height:100%;
+            position:absolute;
+            top:0;
+            left:0;
+            background:linear-gradient(to right, rgba(79,172,254,.8),rgba(0,242,254,.8));
+            box-shadow:.5rem .5rem 3rem 1px rgba(0,0,0,.05);
+            opacity:1;
+            border-radius:.8rem;
+        }
+        .blog-post_date span
+        {
+            display:block;
+            color:#DA0037;
+            font-size:3.0rem;
+            font-weight:600;
+            margin:.5rem 0;
+            opacity:1;
+            font-family: 'Yanone Kaffeesatz', sans-serif; 
+        }
+        .blog-post_title
+        {
+            font-size:3.5rem;
+            margin:1.5rem 0 2rem;
+            color:white;
+            opacity:1;
+            font-family: 'Lobster', cursive;
+        }
+        .blog-post_text
+        {
+            margin-bottom:3rem;
+            font-size:2.5rem;
+            color:azure;
+            opacity:1;
+            font-family: 'Yanone Kaffeesatz', sans-serif; 
+        }
+        .blog-post_cta
+        {
+            display:inline-block;
+            background:darkred;
+            padding:1.5rem 3rem;
+            letter-spacing:1px;
+            text-transform:uppercase;
+            font-size:1.2rem;
+            color:#fff;
+            border-radius:.8rem;
+            font-family: 'Dancing Script', cursive;
+            text-decoration:none;
+            border-color:darkred;
+            opacity:1;
+
+        }
+        .blog-post_cta:hover
+        {
+           border-color:white;
+           background:#DA0037;
+           transition:.9s;
+           color:antiquewhite;
+           opacity:1;
+        }
+
 
      
 
@@ -233,13 +396,7 @@ session_start();
 <!--/head-->
 
 
-<<<<<<< HEAD
 <body>
-    <!--/head-->
-
-
-=======
->>>>>>> d3cd0bd56ab74c187d7026086aad54b3242a9184
 
     <header id="header">
         <div class="container">
@@ -254,6 +411,9 @@ session_start();
                             <li><a href=""><i class="fa fa-linkedin"></i></a></li>
                         </ul>
                     </div>
+                    <div class="logo pull-left">
+                    <h2>Lights Camera Action</h2>
+                    </div>
                 </div>
             </div>
         </div>
@@ -262,8 +422,7 @@ session_start();
         <div class="menu-bar">
             <ul>
                 <li class="active"><a href='http://localhost/302/homee.php'><i class="fa fa-home"></i></i> Home</a></li>
-                <li><a href="https://localhost/302/login_admin.php"><i class="fa fa-check"></i> Admin Login</a>
-            </li>
+                <li><a href="https://localhost/302/login_admin.php"><i class="fa fa-check"></i> Admin Login</a> </li>
                 <li><a href='#'><i class="fa fa-play-circle" ></i> Showtime</a>
                     <div class="sub-menu-1">
                         <ul>
@@ -297,48 +456,35 @@ session_start();
                 
                 </li>
             </ul>
-        </div>
+        </div >
 
     </header>
+    <br>
+    <br>
     <!-----------hhhhhhhhhhhhhhhhhhhhhhhhhhhhhh------------------->
     <!-----------hhhhhhhhhhhhhhhhhhhhhhhhhhhhhh------------------->
 
     <body>
-        <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
-            <div class="carousel-indicators">
-                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active"
-                    aria-current="true" aria-label="Slide 1"></button>
-                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1"
-                    aria-label="Slide 2"></button>
-                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2"
-                    aria-label="Slide 3"></button>
-            </div>
-            <div class="carousel-inner">
-                <div class="carousel-item active">
-                    <img src="images/merchandise/m1.jpg" class="d-block w-100" alt="...">
-                </div>
-                <div class="carousel-item">
-                    <img src="images/merchandise/m2.jpg" class="d-block w-100" alt="...">
-                </div>
-                <div class="carousel-item">
-                    <img src="images/merchandise/m3.jpg" class="d-block w-100" alt="...">
-                </div>
-            </div>
-            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators"
-                data-bs-slide="prev">
-                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span class="visually-hidden">Previous</span>
-            </button>
-            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators"
-                data-bs-slide="next">
-                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                <span class="visually-hidden">Next</span>
-            </button>
-        </div>
-        <br>
-        <br>
-        <br>
-        <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <div class="banner-area">
+       <div class="banner-text">
+       <br>
+       <br>
+       <br>
+       <h2>Here we do not think.We are thought...</h2>
+       <br>
+       <br>
+       <p>For Us, there is nothing more valuable than how people feel in a movie theater about a movie.If you are in a movie theater, you can look two people down and they are laughing while you are laughing or you can look three people down and they love that song that you love. It is living proof that you are not alone.</p>
+       <a href="">Know About Us</a>
+       <a href="">Our Movies</a>
+
+       
+       
+       </div>
+       </div>
 
 
 
@@ -398,128 +544,44 @@ session_start();
           </div>
         </div>
         <br>
-<br>
-<br>
-<br>
-<br>
-        
-        <div style="text-align:center; "><h1 style="font-size: 50px;font-weight:bold; font-family: 'Oswald', sans-serif;">Checkout or new Products</h1></div>
+        <br>
+        <br>
+        <br>
 
-<br>
-<br>
-<br>
-<br>
-<br>
-
-
-            <!-----slider-->
-    <div class="container">
-        
-        
-       
-        <div class="row">       
-
-          <div class="col-md-12">
-            <div class="carousel slide multi-item-carousel" id="theCarousel">
-              <div class="carousel-inner">
-                  <div class="item active">
-                    
-                   <div class="col-xs-4"><a href="#1"><img src="images/mm1.jpg" class="img-responsive" width="400" height="200"></a></div>
-                   </div>
-                   <div class="item">
-                     <div class="col-xs-4"><a href="#1"><img src="images/mm2.jpg" class="img-responsive" width="400" height="200"></a></div>
-                    </div>
-                    <div class="item">
-                     <div class="col-xs-4"><a href="#1"><img src="images/mm7.jpg" class="img-responsive" width="400" height="200"></a></div>
-                    </div>
-                    <div class="item">
-                     <div class="col-xs-4"><a href="#1"><img src="images/mm4.jpg" class="img-responsive" width="400" height="200"></a></div>
-                    </div>
-                    <div class="item">
-                     <div class="col-xs-4"><a href="#1"><img src="images/mm5.png" class="img-responsive" width="400" height="200"></a></div>
-                    </div>
-                    <div class="item">
-                      <div class="col-xs-4"><a href="#1"><img src="images/mm6.jpg" class="img-responsive" width="400" height="200"></a></div>
-                    </div>
-
-
-
-
-
-                <!-- add  more items here -->
-                <!-- Example item start:  -->
-                
-                    <div class="item">
-                      <div class="col-xs-4"><a href="#1"><img src="images/mm4.jpg" class="img-responsive" width="400" height="200"></a></div>
-                    </div>
-                
-                <!--  Example item end -->
-                </div>
-              <a class="left carousel-control" href="#theCarousel" data-slide="prev"><i class="glyphicon glyphicon-chevron-left"></i></a>
-              <a class="right carousel-control" href="#theCarousel" data-slide="next"><i class="glyphicon glyphicon-chevron-right"></i></a>
-           </div>
-         </div>
-       </div>
-     </div>
-       <br>
-       <br>
-       <br>
-       <br>
-
-
-
-       <div style="text-align:center; "><h1 style="font-size: 50px;font-weight:bold; font-family: 'Oswald', sans-serif;">Checkout our foods</h1></div>
-         <!-----slider-->
-    <div class="container">
+      <div class="blog-post">
+      <div class="blog-post_img">
+      <img src="images/portfolio/4.jpg" alt="">
+      </div>
+      <div class="blog-post_info">
+      <div class="blog-post_date">
+      <span>Do Not Forget to check out our</span>
+      </div>
+      <h1 class="blog-post_title">Refreshement Corner</h1>
+      <p class="blog-post_text">The movies just wouldn't be the same without an ice-cold, refreshing pepsi and freshly popped popcorn. Now, classics like Pop Chicken and candy are joined with more menu options than ever. </p>
+      <a href="http://localhost/302/portfolio.php" class="blog-post_cta">Explore More</a>
+      </div>
+      </div>
+      <br>
+      <br>
+        <br>
+        <br>
+        <br>
+      <div class="blog-post">
+      <div class="blog-post_img">
+      <img src="images/merchandise/mm4.jpg" alt="">
+      </div>
+      <div class="blog-post_info">
+      <div class="blog-post_date">
+      <span>We have all types of</span>
+      </div>
+      <h1 class="blog-post_title">Merchandise Product</h1>
+      <p class="blog-post_text">As soon as you go into merchandising, everyone nods sagely and says, 'Ah, now we know why you are doing it.'</p>
+      <a href="http://localhost/302/merch.php" class="blog-post_cta">Explore More</a>
+      </div>
+      </div>
+ 
         
         
-       
-        <div class="row">       
-
-          <div class="col-md-12">
-            <div class="carousel slide multi-item-carousel" id="theCarousel">
-              <div class="carousel-inner">
-                <div class="item active">
-                    
-                  <div class="col-xs-4"><a href="#1"><img src="images/portfolio/1.jpg" class="img-responsive" width="400" height="200"></a></div>
-                </div>
-                <div class="item">
-                  <div class="col-xs-4"><a href="#1"><img src="images/portfolio/2.jpg" class="img-responsive" width="400" height="200"></a></div>
-                </div>
-                <div class="item">
-                  <div class="col-xs-4"><a href="#1"><img src="images/portfolio/3.jpg" class="img-responsive" width="400" height="200"></a></div>
-                </div>
-                <div class="item">
-                  <div class="col-xs-4"><a href="#1"><img src="images/portfolio/4.jpg" class="img-responsive" width="400" height="200"></a></div>
-                </div>
-                <div class="item">
-                  <div class="col-xs-4"><a href="#1"><img src="images/portfolio/5.jpg" class="img-responsive" width="400" height="200"></a></div>
-                </div>
-                <div class="item">
-                  <div class="col-xs-4"><a href="#1"><img src="images/portfolio/6.jpg" class="img-responsive" width="400" height="200"></a></div>
-                </div>
-
-
-
-
-
-                <!-- add  more items here -->
-                <!-- Example item start:  -->
-                
-                <div class="item">
-                  <div class="col-xs-4"><a href="#1"><img src="images/portfolio/3.jpg" class="img-responsive" width="400" height="200"></a></div>
-                </div>
-                
-                <!--  Example item end -->
-              </div>
-              <a class="left carousel-control" href="#theCarousel" data-slide="prev"><i class="glyphicon glyphicon-chevron-left"></i></a>
-              <a class="right carousel-control" href="#theCarousel" data-slide="next"><i class="glyphicon glyphicon-chevron-right"></i></a>
-              </div>
-            </div>
-        </div>
-    </div>
-
-
 
 
 <!--Footer Starts-->
@@ -593,6 +655,7 @@ session_start();
     </body>
 </body>
 </body>
+
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js"
     integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4"
     crossorigin="anonymous"></script>
